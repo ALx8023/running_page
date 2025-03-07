b6073b74834f2198933b605872b51ef2e04ea07d
Lollipop <wenqing7879@qq.com>
2025-03-07 13:51:16 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,17 +11,17 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
-  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+  siteUrl: 'https://www.xiaohongshu.com/user/profile/6661802200000000070051e6?xsec_token=YB9tHNSW7r-KkflRKr-mtaOGMMIGnLmdwLMGy0JnPz1zE=&xsec_source=app_share&xhsshare=CopyLink&appuid=6661802200000000070051e6&apptime=1741326635&share_id=df270278169b47a7ada964c30c7e9573',
+  logo: 'https://sns-avatar-qc.xhscdn.com/avatar/1040g2jo31acknrjhn2005pj1g0h1okf6ntprsd8?imageView2/2/w/540/format/webp|imageMogr2/strip2',
   description: 'Personal site and blog',
   navLinks: [
     {
       name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      url: 'https://www.xiaohongshu.com/user/profile/6661802200000000070051e6?xsec_token=YB9tHNSW7r-KkflRKr-mtaOGMMIGnLmdwLMGy0JnPz1zE=&xsec_source=app_share&xhsshare=CopyLink&appuid=6661802200000000070051e6&apptime=1741326635&share_id=df270278169b47a7ada964c30c7e9573',
     },
     {
       name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      url: 'https://www.xiaohongshu.com/user/profile/6661802200000000070051e6?xsec_token=YB9tHNSW7r-KkflRKr-mtaOGMMIGnLmdwLMGy0JnPz1zE=&xsec_source=app_share&xhsshare=CopyLink&appuid=6661802200000000070051e6&apptime=1741326635&share_id=df270278169b47a7ada964c30c7e9573',
     },
   ],
 };
